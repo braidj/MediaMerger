5948060459f4293375d1827e83a22f43186c254b
Jason Braid <braidj@gmail.com>
2018-03-19 17:16:39 +0000

    Added first TODO

--- MediaMerger.py
+++ MediaMerger.py
@@ -1,13 +1,16 @@
-# Will start to file this out
-
 from ruamel import yaml
 
 
 
 if __name__ == "__main__":
+    #-----------------------------------------------------------------------------
+    # Purpose: main code block called
+    #-----------------------------------------------------------------------------
 
     print("Following config is being used")
     config_file = r"D:\Dropbox\Jason\Development\Python\yaml\media_merger_cfg.yml"
+
+    # TODO Add a list to the YAML file and see how it is stored.
 
     with open(config_file, 'r') as fp:
         config_dict = yaml.load(fp, Loader=yaml.Loader)
